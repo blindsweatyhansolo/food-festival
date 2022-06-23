69e90e7f7c942cc5b07ecb5a441ac5149b621584
J Semidey <mtsemidey@gmail.com>
2022-06-23 13:48:43 -0400

    utilized webpack-file-analyzer and image loader for image files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,62 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 //  basic configuration, webpack needs three properties: entry, output, and mode
 // entry: root of the bundle and the beginning of the dependency graph
 // output: bundles and outputs entry point to specified folder (BP: 'dist')
 // mode: set up the mode in which the webpack runs (default: production)
 // plugins: tells webpack which plugins to look for on load
-module.exports = {
-  entry: './assets/js/script.js',
+
+const config = {
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        // regex to process any image with .jpg file extension
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath(url) {
+                // replaces "../" in require statement with "/assets/"
+                return url.replace("../", "/assets/");  
+              }
+            }
+          },
+          {
+            // make sure this is defined AFTER file-loader processes the images
+            loader: "image-webpack-loader"
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
     }),
+    new BundleAnalyzerPlugin({
+      // the report outputs to an HTML file in the dist folder [report.html]
+      analyzerMode: "static"
+    })
   ],
   mode: 'development'
 };
 
+module.exports = config;
